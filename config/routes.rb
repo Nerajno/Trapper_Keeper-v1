f5e2f4e1a231f5fc1942a0582fd0698ef48f825c
Nerando Johnson <nerajno@gmail.com>
2023-01-12 10:24:23 -0500

    Updated current log

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   resources :friends
 
   get 'home/about' # Original Route home page
+
+  # TODO: Look back into this // Reference :  https://fly.io/docs/rails/getting-started/
   # root 'home#index'
-  root 'friends#index'
+  # root 'friends#index'
+
+  root "root#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
